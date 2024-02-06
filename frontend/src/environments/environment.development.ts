392dbdb01509349d67df3d1be81a6672d6d44de7
Bushogun <sotto_j@hotmail.com>
2024-02-05 23:21:55 -0500

    editing environments

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,3 +1,8 @@
 export const environment = {
-
+    API_URL:'https://localhost:7244/api',
+    API_GET_SELLS:'/Ventas',
+    API_CREATE_SELL:'/Ventas',
+    API_GET_SPECIFIC_SELL:'/Ventas',
+    API_EDIT_SELL:'/Ventas',
+    API_DELETE_SELL:'/Ventas'
 };
